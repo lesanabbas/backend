a4a64b626078cdf5076929fa61d80d34f72c788a
Lesan Abbas <63160640+lesanabbas@users.noreply.github.com>
2024-06-10 20:19:47 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import JsonResponse
+
+def about(request):
+    return JsonResponse({"status": True})
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("about/", about)
 ]
