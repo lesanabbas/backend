73b2c30b7daea4822f47b9ace986b7c97ac04cf8
lesanabbas <lesanabbas@gmail.com>
2024-06-11 12:16:37 +0530

    update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 echo "Running custom script"
 
 # Build steps
+pip3 uninstall django asgiref python-decouple sqlparse tzdata
+pip3 install django asgiref python-decouple sqlparse tzdata
+
+
 pip3 install -r requirements.txt
 python3 manage.py migrate
 
